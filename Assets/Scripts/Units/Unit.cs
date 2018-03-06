e4d10136b5670b4f8fd733714bf4091b4baadc3e
PYZILEND <33196585+PYZILEND@users.noreply.github.com>
2018-03-06 11:52:15 +0800

    Ignorethis

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+    int maxHealth;
+    int currentHealth;
+
+    int range;
+    int damage;
+
+    int maxMoveSpeed;
+    int moveSpeed;
+
+    int cost;
+
+    public void MoveToCell(LogicalMapCell destination)
+    {
+
+    }
+
+    public void ShootAt(LogicalMapCell cell)
+    {
+
+    }
+
+    public void Destroy()
+    {
+
+    }
+}
